a907ae363ddce28856bde55a81aac9ec11201f9b
YannPne <yann.panarese@gmail.com>
2025-04-30 15:29:03 +0200

    merge

--- src/transcendence/urls.py
+++ src/transcendence/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for transcendance project.
+URL configuration for transcendence project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pong.urls')),  # Cette ligne inclut les URLs de l'app game
+    path('', include('core.urls')),  # Cette ligne inclut les URLs de l'app game
 ]
 
