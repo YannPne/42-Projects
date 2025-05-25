8deca440b7ce84075f1ed678834a90b615bbac1a
DailyCraft <51261780+DailyCraft@users.noreply.github.com>
2025-05-25 15:45:36 +0200

    Fix docker run

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import * as fs from "node:fs";
 import { resolve } from "node:path";
 
-export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        ws: true
+export default defineConfig(env => {
+  const config: UserConfig = {
+    plugins: [
+      tailwindcss()
+    ]
+  };
+
+  if (env.command == "serve") {
+    config.server = {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          ws: true
+        }
+      },
+      https: {
+        cert: fs.readFileSync(resolve(__dirname, "fullchain.crt")),
+        key: fs.readFileSync(resolve(__dirname, "privkey.key"))
       }
-    },
-    https: {
-      cert: fs.readFileSync(resolve(__dirname, "fullchain.crt")),
-      key: fs.readFileSync(resolve(__dirname, "privkey.key"))
-    }
+    };
   }
+
+  return config;
 });
